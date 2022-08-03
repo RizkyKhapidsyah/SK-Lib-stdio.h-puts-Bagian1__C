4aefe157962ebfca04ac41c00da287ba15b690fa
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-08-03 20:44:54 +0700

    Initial commit

---
+++ SK-Lib-stdio.h-puts-Bagian1__C/Source.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+/*
+    Source by CPlusPlus (https://www.cplusplus.com)
+    Modified For Learn by RK
+    I.D.E : VS2022
+*/
+
+int main() {
+	char string[] = "Hello world!";
+	puts(string);
+
+    _getch();
+    return 0;
+}
